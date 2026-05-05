a770b5512464d3351212d0aac4b8734b6c7c0b66
charulatha3 <charulathakumaresan3@gmail.com>
2026-05-06 00:23:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,26 @@ if st.button("Predict"):
     input_data = np.array([[age, balance]])
     prediction = model.predict_proba(input_data)[0][1]
     st.write(f"Churn Probability: {prediction:.2f}")
+credit_score = st.number_input("Credit Score")
+geography = st.selectbox("Geography", ["France", "Spain", "Germany"])
+gender = st.selectbox("Gender", ["Male", "Female"])
+tenure = st.slider("Tenure", 0, 10)
+num_products = st.slider("Number of Products", 1, 4)
+is_active = st.selectbox("Active Member", [0, 1])
+salary = st.number_input("Estimated Salary")
+geo_france = 1 if geography == "France" else 0
+geo_spain = 1 if geography == "Spain" else 0
+
+gender_male = 1 if gender == "Male" else 0
+input_data = np.array([[credit_score, age, tenure, balance,
+                        num_products, is_active, salary,
+                        geo_france, geo_spain, gender_male]])
+
+                        if st.button("Predict"):
+    prob = model.predict_proba(input_data)[0][1]
+
+    if prob > 0.5:
+        st.error(f"⚠️ High Churn Risk: {prob:.2f}")
+    else:
+        st.success(f"✅ Low Churn Risk: {prob:.2f}")
+        
